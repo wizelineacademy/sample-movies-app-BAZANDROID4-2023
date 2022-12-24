54d17e4c372a6f9706e24d3c1654000ef4808d86
Diego Parra <diego.parra@wizeline.com>
2022-12-24 08:07:34 -0500

    hilt, navigation, viewBinding setup

---
+++ app/src/main/java/com/wizeline/coroutinesexercises/CoroutinesApp.kt
@@ -0,0 +1,7 @@
+package com.wizeline.coroutinesexercises
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class CoroutinesApp: Application()
---
+++ app/src/main/java/com/wizeline/coroutinesexercises/ui/home/HomeFragment.kt
@@ -0,0 +1,29 @@
+package com.wizeline.coroutinesexercises.ui.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.wizeline.coroutinesexercises.databinding.FragmentHomeBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class HomeFragment : Fragment() {
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        _binding = null
+        super.onDestroyView()
+    }
+}
