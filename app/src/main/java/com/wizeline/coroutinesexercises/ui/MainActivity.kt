bb717e7b11aa5db0c89d269f0a2d0c71fbe1820c
Diego Parra <diego.parra@wizeline.com>
2022-12-24 08:07:34 -0500

    hilt, navigation, viewBinding setup

--- app/src/main/java/com/wizeline/coroutinesexercises/ui/MainActivity.kt
+++ app/src/main/java/com/wizeline/coroutinesexercises/ui/MainActivity.kt
@@ -1,8 +1,11 @@
-package com.wizeline.coroutinesexercises
+package com.wizeline.coroutinesexercises.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.wizeline.coroutinesexercises.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
