a0f97e481e83ec79fd3fae4ab4da29935a7e28f5
Diego Parra <diego.parra@wizeline.com>
2022-12-24 08:27:12 -0500

    launch coroutines vs threads

--- app/src/main/java/com/wizeline/coroutinesexercises/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/wizeline/coroutinesexercises/ui/home/HomeViewModel.kt
@@ -1,9 +1,43 @@
 package com.wizeline.coroutinesexercises.ui.home
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import javax.inject.Inject
+import kotlin.concurrent.thread
+
+private const val TAG = "HomeViewModel"
 
 @HiltViewModel
 class HomeViewModel @Inject constructor() : ViewModel() {
+
+    init {
+        launchCoroutines()
+        //launchThreads()
+    }
+
+    private fun launchCoroutines() = viewModelScope.launch {
+        repeat(100_000) {
+            launch {
+                delay(5000L)
+                print(".")
+            }
+        }
+    }
+
+    private fun launchThreads() {
+        repeat(100_000) {
+            thread {
+                Thread.sleep(5000L)
+                print(".")
+            }
+        }
+    }
+
 }
