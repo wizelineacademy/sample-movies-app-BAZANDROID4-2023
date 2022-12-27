a93614bccd383a3e74bbbb87ae76c3fc6bc5b5ae
Diego Parra <diego.parra@wizeline.com>
2022-12-27 08:46:42 -0500

    feat: Add getDataAsync implementation

--- app/src/main/java/com/wizeline/coroutinesexercises/domain/usecases/GetGenresWithMoviesUseCase.kt
+++ app/src/main/java/com/wizeline/coroutinesexercises/domain/usecases/GetGenresWithMoviesUseCase.kt
@@ -3,14 +3,17 @@ package com.wizeline.coroutinesexercises.domain.usecases
 import com.wizeline.coroutinesexercises.domain.entities.GenreWithMovies
 import com.wizeline.coroutinesexercises.domain.repositories.MoviesRepository
 import com.wizeline.coroutinesexercises.utils.logExecutionTimeAndReturnResult
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.coroutineScope
 import javax.inject.Inject
 
 class GetGenresWithMoviesUseCase @Inject constructor(
     private val moviesRepository: MoviesRepository
 ) {
     suspend operator fun invoke(): Result<List<GenreWithMovies>> = runCatching {
-        logExecutionTimeAndReturnResult("getDataSequential") {
-            getDataSequential()
+        logExecutionTimeAndReturnResult {
+            getDataAsync()
         }
     }
 
@@ -23,7 +26,25 @@ class GetGenresWithMoviesUseCase @Inject constructor(
     }
 
     private suspend fun getDataAsync(): List<GenreWithMovies> {
-        //  TODO:   Create an implementation to run coroutines in parallel and compare execution times
-        return getDataSequential()
+        val genres = moviesRepository.getGenres()
+        val data = coroutineScope {
+            genres.map { genre ->
+                async {
+                    val movies = moviesRepository.getMoviesByGenre(genre.id)
+                    GenreWithMovies(genre, movies)
+                }
+            }
+        }
+        return data.awaitAll()
     }
 }
+
+/*
+Execution times:
+    MoviesRepositoryImpl:
+        getDataSequential:  3823 ms
+        getDataASync:       2527 ms
+    FakeMoviesRepository (consistent times as there is no cache):
+        getDataSequential:  8137 ms
+        getDataAsync:       4070 ms
+ */
