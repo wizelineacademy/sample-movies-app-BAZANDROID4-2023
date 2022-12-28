f89d7304185c07ce97eb02e6a927fd7d4d441937
Diego Parra <diego.parra@wizeline.com>
2022-12-28 18:24:16 -0500

    refactor: Add hot observable to manage state and avoid starting observable from scratch.

--- app/src/main/java/com/wizeline/coroutinesexercises/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/wizeline/coroutinesexercises/ui/home/HomeViewModel.kt
@@ -3,9 +3,12 @@ package com.wizeline.coroutinesexercises.ui.home
 import androidx.lifecycle.ViewModel
 import com.wizeline.coroutinesexercises.di.MainScheduler
 import com.wizeline.coroutinesexercises.domain.usecases.GetGenresWithMoviesUseCase
+import com.wizeline.coroutinesexercises.utils.update
 import dagger.hilt.android.lifecycle.HiltViewModel
 import io.reactivex.rxjava3.core.Flowable
 import io.reactivex.rxjava3.core.Scheduler
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+import io.reactivex.rxjava3.processors.BehaviorProcessor
 import javax.inject.Inject
 
 @HiltViewModel
@@ -14,20 +17,37 @@ class HomeViewModel @Inject constructor(
     @MainScheduler private val mainScheduler: Scheduler
 ) : ViewModel() {
 
-    val uiState: Flowable<HomeUiState> = getGenresWithMoviesUseCase()
-        .toFlowable()
-        .map {
-            HomeUiState(
-                isLoading = false,
-                genreSections = it
-            )
-        }
-        .onErrorReturn {
-            HomeUiState(
-                isLoading = false,
-                errorMessage = it.message
-            )
-        }
-        .startWithItem(HomeUiState(isLoading = true))
-        .observeOn(mainScheduler)
+    private val _uiState = BehaviorProcessor.createDefault(HomeUiState())
+    val uiState: Flowable<HomeUiState> = _uiState
+    private val compositeDisposable = CompositeDisposable()
+
+    init {
+        refreshMovies()
+    }
+
+    private fun refreshMovies() {
+        val moviesResult = getGenresWithMoviesUseCase()
+        moviesResult
+            .observeOn(mainScheduler)
+            .doOnSubscribe { _uiState.update { it?.copy(isLoading = true) } }
+            .subscribe(
+                { data ->
+                    _uiState.update {
+                        it?.copy(isLoading = false, genreSections = data)
+                    }
+                }, { e ->
+                    _uiState.update {
+                        it?.copy(
+                            isLoading = false,
+                            errorMessage = "Couldn't load data: ${e.message}"
+                        )
+                    }
+                }
+            ).let { compositeDisposable.add(it) }
+    }
+
+    override fun onCleared() {
+        compositeDisposable.clear()
+        super.onCleared()
+    }
 }
