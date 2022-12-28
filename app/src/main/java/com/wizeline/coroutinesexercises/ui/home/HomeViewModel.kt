9b2235c96888e365ae0de4f89aa65c8ab9d5776a
Diego Parra <diego.parra@wizeline.com>
2022-12-28 18:21:46 -0500

    refactor: Add publish/replay to avoid starting observable from scratch.

--- app/src/main/java/com/wizeline/coroutinesexercises/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/wizeline/coroutinesexercises/ui/home/HomeViewModel.kt
@@ -6,6 +6,8 @@ import com.wizeline.coroutinesexercises.domain.usecases.GetGenresWithMoviesUseCa
 import dagger.hilt.android.lifecycle.HiltViewModel
 import io.reactivex.rxjava3.core.Flowable
 import io.reactivex.rxjava3.core.Scheduler
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+import timber.log.Timber
 import javax.inject.Inject
 
 @HiltViewModel
@@ -14,8 +16,11 @@ class HomeViewModel @Inject constructor(
     @MainScheduler private val mainScheduler: Scheduler
 ) : ViewModel() {
 
+    private val compositeDisposable = CompositeDisposable()
+
     val uiState: Flowable<HomeUiState> = getGenresWithMoviesUseCase()
         .toFlowable()
+        .doOnEach { Timber.d("doOnEach before replay!, state = ${it.value}") }
         .map {
             HomeUiState(
                 isLoading = false,
@@ -30,4 +35,12 @@ class HomeViewModel @Inject constructor(
         }
         .startWithItem(HomeUiState(isLoading = true))
         .observeOn(mainScheduler)
+        .replay(1)
+        .autoConnect(1) { compositeDisposable.add(it) }
+        .doOnEach { Timber.d("doOnEach after replay!, state = ${it.value}") }
+
+    override fun onCleared() {
+        compositeDisposable.clear()
+        super.onCleared()
+    }
 }
